c6c5dcaa2aa5129f19f368614cef2dad56df4500
Piotr Antas <antas.piotr@gmail.com>
2017-02-12 18:10:28 +0100

    comments delete

--- scripts.js
+++ scripts.js
@@ -1,45 +1,45 @@
 
-var url = 'http://api.icndb.com/jokes/random'; // zmienna trzymająca adres (nie trzeba go póżniej wpisywać)
+var url = 'http://api.icndb.com/jokes/random'; 
 
-var button = document.getElementById('get-joke'); // zmienna pobierająca przycisk 
-button.addEventListener('click', function(){  //nasłuchiwanie przycisku  - po kliknięciu wykona getJoke
+var button = document.getElementById('get-joke'); 
+button.addEventListener('click', function(){  
   getJoke();
 });
 
-window.onload = getJoke(); // wykona getJoke przy ładowaniu strony/okna 
+window.onload = getJoke(); 
 
-var paragraph = document.getElementById('joke'); //pobiera paragraf z html by go dalej wykorzystać
+var paragraph = document.getElementById('joke'); 
 
 function getJoke() {
-  var xhr = new XMLHttpRequest(); // tworzymy nową instancję obiektu XMLHttpRequet() czyli zapytanie, zamykając ją w zmiennej
-  xhr.open('GET', url); //na zmiennej trzymającej zapytanie wykonujemy metodę open i nadajemy jej parametry - 'GET' (metoda protokołu HTTP, która pobiera zasoby z serwera) i url (nasza adres serwera z którego chcemy pobrać dane, gdybyśmy wcześniej nie utworzyli zmiennej "url" to wpisalibyśmy cały adres. w tym przypadku jest też rzecia zmienna,która poprzez true lub false okresla czy metoda open wykana się synchornicznie(false) czy asynchronicznie(true), domyślnie jest ustawine true, więc tutaj, niczego nie pisząc, okreslamy metodę jako asynchroniczną - nie wstrzyma ona działania pozostałej części kodu)
-  xhr.addEventListener('load', function(){  //na zmiennej  trzymającej zapytanie ustawiamy teraz nasłuchiwacz odpowiedzi, który po jej załadowaniu (load) odpali funkcję
-    var odp = JSON.parse(xhr.response); // w naszej funkcji, która wykona się po odpowiedzi serwera i jej załadowaniu (load) tworzymy zmieną odp, która trzyma odpowiedź serwera. Odpowiedź ta jest  formacie JSON, by ją wykoryzstać w js musimy ją sparsować, czyli wykonać na niej metodę (specjalna, wbudowana w JSON) .parse, która zmienia to co w JSON w zrozumiały dla js obiekt. Czyli po kolei - JSONowy obiekt odpowiedzi parsujemy biorąc atrybut response (trzymający w sobie samą odpowiedź) z zmiennej xhr trzymającej całe zapytanie (czyli w tym i odpowiedź). W ten sposób zmienna odp w rezultanice trzyma odpowiedź serwera przygotowaną do manipulacji w js - możemy dalej coś z nią robić
-    paragraph.innerText = odp.value.joke; //w zmienną paragraph, pd którą wczesniej zamkneliśmy paragraf ze strony wstawiamy wartość dowcipu. Wartośc ta, zamknięta jest w zmiennej odp odpowienia wyżej przygotowanej do przetwarzania. Zmienna odp ma dwa klucze - type i value (możemy je podejżeć w Network, gdzie odpowiedź jest już sparsowana przez przeglądarkę, niezależnie od naszego parsowania, więc i tak musimy je zrobić), tam widzimy, że tym co trzyma treść dowcipu na którym nam zalezy jest categoria joke. Zatem by do tej treści się dostać i przypisać ją do treści paragrafu musimy wziąć odpowiedź (odp) i wniej po kolei dotrzeć do value i joke. W ten sposób w paragrafie wyświetli nam się to co jest w joke
+  var xhr = new XMLHttpRequest(); 
+  xhr.open('GET', url); 
+  xhr.addEventListener('load', function(){  
+    var odp = JSON.parse(xhr.response); 
+    paragraph.innerText = odp.value.joke; 
   });
-  xhr.send(); // wysyłamy nasze zapytanie 
+  xhr.send(); 
 }
 
 
 
 /*
 //wersja jquery
-var url = 'http://api.icndb.com/jokes/random'; //to co wyżej
+var url = 'http://api.icndb.com/jokes/random'; 
 
-var $button = $('#get-joke').click(function() { //jak wyżej: twrzymy jQuerową zmienną i wstawiamy w nią - weź get-joke (button z takim id) i po kliknięciu w niego wykonaj funcję getJoke
+var $button = $('#get-joke').click(function() { 
 	getJoke();
 });
 
-window.onload = getJoke(); // to co wyżej
+window.onload = getJoke(); 
 
-var $paragraph = $('#joke'); // jak wyżej: twrzymy jQuerową zmienną i wstawiamy w nią - weź joke (paragraf z takim id)
+var $paragraph = $('#joke'); 
 
 function getJoke() {
-$.ajax({                           // tworzymy jquerową metodę ajaxa wpisując w nią obiekt js
-	method: 'GET',                   // określamy metodę (zamiast open) i wpisujemy jak ma być - w tym przypadku GET, czyli pobranie zasobów z serwera
-	url: url,                        // określamy żródło/serwer i wpisujemy adres www, w tym przypadku zamknięty pod zmienną url
-	success: function(sparsowanaOdpowiedzSerwera) {         // okreslamy co ma się stać w przypadku udanego (sakces!) wysłania i odebrania danych z serwera. Nie musimy parsować bo jQuery robi to za nas i te sparsowane dane znajdują się parametrze funkcji (zawsze) - określamy "nazwę" parametru funkcji by póżniej móc się do tych danych dostać
-		$paragraph.text(sparsowanaOdpowiedzSerwera.value.joke); // w zmiennej $paragraph wstawiamy przy uzyciu metody .text to co znajdziemy w odpowiedzi serwera (zamkniętej w parametrze funkcji) - a tam tak jak wyżej jest klucz value a w nim joke, który trzyma treść naszego dowcipu
+$.ajax({  
+	method: 'GET',  
+	url: url,   
+	success: function(sparsowanaOdpowiedzSerwera) {   
+		$paragraph.text(sparsowanaOdpowiedzSerwera.value.joke); 
 	}
 
 });
